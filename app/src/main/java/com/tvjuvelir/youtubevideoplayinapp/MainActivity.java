f7308405dab19c17a720f44a9159b750a9c947a3
Denis Pimkin <pimkinden@gmail.com>
2019-03-04 00:44:38 +0300

    get youtube link part 1

--- app/src/main/java/com/tvjuvelir/youtubevideoplayinapp/MainActivity.java
+++ app/src/main/java/com/tvjuvelir/youtubevideoplayinapp/MainActivity.java
@@ -5,6 +5,15 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.SearchListResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,11 +27,50 @@ public class MainActivity extends AppCompatActivity {
             fragment = createMainFragment();
             fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
         }
+        //getKey();
     }
 
     protected Fragment createMainFragment()
     {
         return MainFragment.newInstance();
         //getSupportFragmentManager().beginTransaction().replace(R.id.video_container, myFragment).commit();
+    }
+
+    public void getKey()
+    {
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try  {
+                    YouTube youtube = new YouTube.Builder(new NetHttpTransport(),
+                            new JacksonFactory(), new HttpRequestInitializer() {
+                        @Override
+                        public void initialize(HttpRequest hr) {}
+                    }).setApplicationName("Ювелирочка").build();
+                    HashMap<String, String> parameters = new HashMap<>();
+                    parameters.put("part", "snippet");
+                    parameters.put("maxResults", "50");
+                    parameters.put("q", "Ювелирочка ТВ");
+                    parameters.put("type", "");
+
+                    YouTube.Search.List searchListByKeywordRequest = youtube.search().list(parameters.get("part"));
+                    if (parameters.containsKey("maxResults")) {
+                        searchListByKeywordRequest.setMaxResults(Long.parseLong(parameters.get("maxResults")));
+                    }
+                    if (parameters.containsKey("q") && parameters.get("q") != "") {
+                        searchListByKeywordRequest.setQ(parameters.get("q"));
+                    }
+                    if (parameters.containsKey("type") && parameters.get("type") != "") {
+                        searchListByKeywordRequest.setType(parameters.get("type"));
+                    }
+                    SearchListResponse response = searchListByKeywordRequest.execute();
+                    System.out.println(response);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
     }
 }
