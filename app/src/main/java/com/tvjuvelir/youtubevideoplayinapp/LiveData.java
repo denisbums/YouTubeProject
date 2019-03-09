2c18ff6483033f9ef6b77168a73fa9a1859f46d9
Denis Pimkin <pimkinden@gmail.com>
2019-03-09 17:29:26 +0300

    Auto find live channel

--- app/src/main/java/com/tvjuvelir/youtubevideoplayinapp/LiveData.java
+++ app/src/main/java/com/tvjuvelir/youtubevideoplayinapp/LiveData.java
@@ -5,9 +5,13 @@ import android.os.AsyncTask;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.Json;
+import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.ResourceId;
 import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
 
 import java.util.HashMap;
 
@@ -24,6 +28,7 @@ public class LiveData extends AsyncTask<Void, Void, String> {
 
     @Override
     protected String doInBackground(Void... voids) {
+        String returnValue = "";
         try {
             YouTube youtube = new YouTube.Builder(new NetHttpTransport(),
                     new JacksonFactory(), new HttpRequestInitializer() {
@@ -31,28 +36,21 @@ public class LiveData extends AsyncTask<Void, Void, String> {
                 public void initialize(HttpRequest hr) {
                 }
             }).setApplicationName("Ювелирочка").build();
-            HashMap<String, String> parameters = new HashMap<>();
-            parameters.put("part", "snippet");
-            parameters.put("maxResults", "50");
-            parameters.put("q", "Ювелирочка ТВ");
-            parameters.put("type", "");
 
-            YouTube.Search.List searchListByKeywordRequest = youtube.search().list(parameters.get("part"));
-            if (parameters.containsKey("maxResults")) {
-                searchListByKeywordRequest.setMaxResults(Long.parseLong(parameters.get("maxResults")));
-            }
-            if (parameters.containsKey("q") && parameters.get("q") != "") {
-                searchListByKeywordRequest.setQ(parameters.get("q"));
-            }
-            if (parameters.containsKey("type") && parameters.get("type") != "") {
-                searchListByKeywordRequest.setType(parameters.get("type"));
-            }
+            YouTube.Search.List searchListByKeywordRequest = youtube.search().list("snippet");
+            searchListByKeywordRequest.setMaxResults(Long.parseLong("50"));
+            searchListByKeywordRequest.setQ("Ювелирочка ТВ");
             searchListByKeywordRequest.setKey(MainActivity.Key);
             SearchListResponse response = searchListByKeywordRequest.execute();
-            //System.out.println(response);
+            for (SearchResult result : response.getItems()) {
+                if(result.getId().getKind().equals("youtube#video") && result.getSnippet().getLiveBroadcastContent().equals("live")) {
+                    returnValue = result.getId().getVideoId();
+                    break;
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return "RLuYziUgP8I";
+        return returnValue;
     }
 }
