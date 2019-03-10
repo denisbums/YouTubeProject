443e680866e732eb29ce20ab6f8d96489bcc0b08
Denis Pimkin <pimkinden@gmail.com>
2019-03-10 13:33:09 +0300

    new channel search

--- app/src/main/java/com/tvjuvelir/youtubevideoplayinapp/LiveData.java
+++ app/src/main/java/com/tvjuvelir/youtubevideoplayinapp/LiveData.java
@@ -32,17 +32,25 @@ public class LiveData extends AsyncTask<Void, Void, String> {
                 }
             }).setApplicationName("Ювелирочка").build();
 
-            YouTube.Search.List searchListByKeywordRequest = youtube.search().list("snippet");
-            searchListByKeywordRequest.setMaxResults(Long.parseLong("5"));
+//            YouTube.Search.List searchListByKeywordRequest = youtube.search().list("snippet");
+//            searchListByKeywordRequest.setMaxResults(Long.parseLong("5"));
+//            searchListByKeywordRequest.setQ("Ювелирочка ТВ");
+//            searchListByKeywordRequest.setKey(MainActivity.Key);
+//            SearchListResponse response = searchListByKeywordRequest.execute();
+//            for (SearchResult result : response.getItems()) {
+//                if(result.getId().getKind().equals("youtube#video") && result.getSnippet().getLiveBroadcastContent().equals("live")) {
+//                    returnValue = result.getId().getVideoId();
+//                    break;
+//                }
+//            }
+            YouTube.Search.List searchListByKeywordRequest = youtube.search().list("id");
+            searchListByKeywordRequest.setEventType("live");
+            searchListByKeywordRequest.setMaxResults(Long.parseLong("1"));
             searchListByKeywordRequest.setQ("Ювелирочка ТВ");
+            searchListByKeywordRequest.setType("video");
             searchListByKeywordRequest.setKey(MainActivity.Key);
             SearchListResponse response = searchListByKeywordRequest.execute();
-            for (SearchResult result : response.getItems()) {
-                if(result.getId().getKind().equals("youtube#video") && result.getSnippet().getLiveBroadcastContent().equals("live")) {
-                    returnValue = result.getId().getVideoId();
-                    break;
-                }
-            }
+            returnValue = response.getItems().get(0).getId().getVideoId();
         } catch (Exception e) {
             e.printStackTrace();
         }
